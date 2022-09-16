9714d83165cb0d21c425dd5641de857f328ef73b
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-16 13:53:40 +0200

    EMPEZANDO

---
+++ funcionesArrays/funciones.php
@@ -0,0 +1,12 @@
+<?php
+    // Crea un array llamado nombres que contenga varios nombres.
+    $nombres = ["Maria", "Carmen" , "Manolo", "Alberto"];
+
+    // Muestra el número de elementos que tiene el array (función count) 
+    echo count($nombres);
+
+    /* Crea una cadena que contenga los nombres de los alumnos existentes en
+     el array separados por un espacio y muéstrala (función de strings implode) */
+
+     $cadena = implode($nombres, );
+?>
