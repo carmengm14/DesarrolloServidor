ed440f5f49a21b255cf17e150136bcccf02fb94b
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-16 12:20:23 +0200

    cambios

--- cadenas.php
+++ cadenas.php
@@ -1,4 +1,15 @@
 <?php
+    /*
+    PARA RELLENAR EL NOMBRE EN EL BUSCADOR PONDREMOS:
+    http://127.0.0.1:8080/cadenas.php?nombre=JUAN
+    y nos saldria de nombre JUAN en vez de Victor.
+    */
     $nombre = $_GET['nombre'] ?? 'Victor';
-    echo $nombre;
+    echo trim($nombre, '/');
+?>
+
+<br>
+
+<?php
+
 ?>
