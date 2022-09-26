6456d4ba1858da1c1f0e0062130da31325fba0c9
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-26 13:53:09 +0200

    h

---
+++ headers/idioma.php
@@ -0,0 +1,4 @@
+<?php
+$idioma = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+echo $idioma;
+?>
---
+++ headers/navegador.php
@@ -0,0 +1,8 @@
+<?php
+header("");
+print "<p>Esta es la página 2</p>";
+
+print "<p>La redirección <strong>NO</strong> se ha realizado</p>";
+
+print "<p><a href=\"cabeceras-header-1-1.php\">Volver a la página 1</a></p>";
+?>
