182e2754dd88c15bf9fd7769e86a0054ac6f4398
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-30 12:06:21 +0200

    prueba

---
+++ headers-respuesta/download.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: text/csv');
+header('Content-Length: 1000000');
+header('Content-Disposition: attachment; filename = "download.zip"');
+for ($i=0; $i < 1000 ; $i++) { 
+    echo str_repeat(".",1000);
+    usleep(50000);
+}
+
+?>
