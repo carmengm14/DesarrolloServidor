698114b38a4d9bd2d246cff6e8c21f8f1a6a025e
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-09-30 11:44:49 +0200

    hecho

--- headers-respuesta/productos.php
+++ headers-respuesta/productos.php
@@ -2,24 +2,12 @@
 $productos = ["1" => "Producto 1", 
                 "2" => "Producto 2", 
                 "3" => "Producto 3"];
-generarCSV($productos);
 
-function generarCSV($array){
-    foreach ($array as $key => $productos) {
-        echo $key," ", $productos, "\n";
-    }
+
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename = "download.csv"');
+foreach($productos as $key => $value){ 
+    echo $key . " = " .$value . "\n";
 }
-?>
 
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="utf-8">
-    <title><?= $title ?></title>
-    <meta name="author" content="Víctor Ponz">
-</head>    
-<body>
-    <h1>DESCARGAR CSV</h1>
-    
-</body>
-</html>
+?>
