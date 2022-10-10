3f7f8eed05ad749b364ec9c9b29d4951af5f36d6
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-10-10 13:41:07 +0200

    hecho

--- symphony/symfony-contactos/src/Controller/ContactoController.php
+++ symphony/symfony-contactos/src/Controller/ContactoController.php
@@ -13,37 +13,14 @@ class ContactoController extends AbstractController
 {
     private $contactos = [
         1 => ["nombre" => "Juan Pérez", "telefono" => "524142432", "email" => "juanp@ieselcaminas.org"],
-        2 => ["nombre" => "Ana López", "telefono" => "58958448", "email" => "anita@ieselcaminas.org"],
+        2 => ["nombre" => "Ana }López", "telefono" => "58958448", "email" => "anita@ieselcaminas.org"],
         5 => ["nombre" => "Mario Montero", "telefono" => "5326824", "email" => "mario.mont@ieselcaminas.org"],
         7 => ["nombre" => "Laura Martínez", "telefono" => "42898966", "email" => "lm2000@ieselcaminas.org"],
         9 => ["nombre" => "Nora Jover", "telefono" => "54565859", "email" => "norajover@ieselcaminas.org"]
     ];    
 
     /**
-    * @Route("/contacto/insertar", name="insertar")
-    */
-    public function insertar(ManagerRegistry $doctrine): Response{
-        $entityManager = $doctrine->getManager();
-        foreach($this->contactos as $c){
-            $contacto = new Contacto();
-            $contacto->setNombre($c["nombre"]);
-            $contacto->setTelefono($c["telefono"]);
-            $contacto->setEmail($c["email"]);
-            $entityManager->persist($contacto);
-        }
-
-        try
-        {
-            //Sólo se necesita realizar flush una vez y confirmará todas las operaciones pendientes
-            $entityManager->flush();
-            return new Response("Contactos insertados");
-        } catch (\Exception $e) {
-            return new Response("Error insertando objetos");
-        }  
-    }
-
-    /**
-    * @Route("/contacto/{codigo}", name="fichacontacto")
+    * @Route("/contacto/{codigo<\d+>?1}", name="fichacontacto")
     */
     public function ficha(ManagerRegistry $doctrine, $codigo): Response{
         $repositorio = $doctrine->getRepository(Contacto::class);
@@ -61,7 +38,7 @@ class ContactoController extends AbstractController
         //Filtramos aquellos que contengan dicho texto en el nombre
         $repositorio = $doctrine->getRepository(Contacto::class);
 
-        $contactos = $repositorio-> findByName($texto);
+        $contactos = $repositorio-> findByNombre($texto);
 
         return $this->render('listacontactos.html.twig', [
             'contactos' => $contactos
@@ -112,6 +89,29 @@ class ContactoController extends AbstractController
             return $this->render('fichacontacto.html.twig', [
                 'contacto' => null
             ]);  
+    }
+
+     /**
+    * @Route("/contacto/insertar", name="insertar")
+    */
+    public function insertar(ManagerRegistry $doctrine): Response{
+        $entityManager = $doctrine->getManager();
+        foreach($this->contactos as $c){
+            $contacto = new Contacto();
+            $contacto->setNombre($c["nombre"]);
+            $contacto->setTelefono($c["telefono"]);
+            $contacto->setEmail($c["email"]);
+            $entityManager->persist($contacto);
+        }
+
+        try
+        {
+            //Sólo se necesita realizar flush una vez y confirmará todas las operaciones pendientes
+            $entityManager->flush();
+            return new Response("Contactos insertados");
+        } catch (\Exception $e) {
+            return new Response("Error insertando objetos");
+        }  
     }
 
 }
