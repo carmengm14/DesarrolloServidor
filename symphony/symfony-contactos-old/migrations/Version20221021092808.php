233c7459bb46d7d9eb3d2cb580be66229f4341fe
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-10-21 11:28:36 +0200

    hecho

---
+++ symphony/symfony-contactos-old/migrations/Version20221021092808.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221021092808 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE nombre');
+        $this->addSql('ALTER TABLE contacto CHANGE provincia_id provincia_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE contacto ADD CONSTRAINT FK_2741493C4E7121AF FOREIGN KEY (provincia_id) REFERENCES provincia (id)');
+        $this->addSql('CREATE INDEX IDX_2741493C4E7121AF ON contacto (provincia_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE nombre (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE contacto DROP FOREIGN KEY FK_2741493C4E7121AF');
+        $this->addSql('DROP INDEX IDX_2741493C4E7121AF ON contacto');
+        $this->addSql('ALTER TABLE contacto CHANGE provincia_id provincia_id INT NOT NULL');
+    }
+}
