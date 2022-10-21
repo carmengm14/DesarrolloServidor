c83fda478215ec73ed0de65c5654aa952fda8f72
carmengm14 <carmen.garcia.monreal@gmail.com>
2022-10-21 12:30:34 +0200

    h

--- symphony/symfony-contactos-old/migrations/Version20221021101024.php
+++ symphony/symfony-contactos-old/migrations/Version20221021101024.php
@@ -20,16 +20,17 @@ final class Version20221021101024 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        /* $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('DROP TABLE nombre');
+    /*
         $this->addSql('ALTER TABLE contacto CHANGE provincia_id provincia_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE contacto ADD CONSTRAINT FK_2741493C4E7121AF FOREIGN KEY (provincia_id) REFERENCES provincia (id)');
         $this->addSql('CREATE INDEX IDX_2741493C4E7121AF ON contacto (provincia_id)');
-     */}
+      */}
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        //this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE nombre (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE user');
         $this->addSql('ALTER TABLE contacto DROP FOREIGN KEY FK_2741493C4E7121AF');
